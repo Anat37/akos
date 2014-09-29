5c07815838551170c286a3fff7511400ebbd20f0
Козлов Никита <s02130160@dazhbog.cmc.msu.ru>
2014-09-29 15:42:05 +0400

    ,astyle --style=ansi main.c' has been applied!

--- task-0/main.c
+++ task-0/main.c
@@ -11,30 +11,36 @@ int main()
     short num_to_hex = 0;
     scanf("%llu",&num);
 
-    while (num){
+    while (num)
+    {
         bin_nums[i] = num%2;
         num>>=1;
         i++;
     }
-    while (i<SIZE){
+
+    while (i<SIZE)
+    {
         bin_nums[i] = 0;
         i++;
     }
- 
-    for(i = SIZE-1;i>=0;i--){
+
+    for(i = SIZE-1; i>=0; i--)
+    {
         printf("%i",bin_nums[i]);
-        if(i%4==0 && i!=0) 
+        if(i%4==0 && i!=0)
             printf(" ");
-                if (i%8==0 && i!=0)
-                    printf("| ");
+        if (i%8==0 && i!=0)
+            printf("| ");
     }
     printf("\n");
-   
-    for(i = SIZE-1;i>=0;i--){
+
+    for(i = SIZE-1; i>=0; i--)
+    {
         num_to_hex += mul*bin_nums[i];
         mul >>= 1;
 
-        if (i%4==0){
+        if (i%4==0)
+        {
             printf("%4hX ",num_to_hex);
             num_to_hex = 0;
             mul = 8;
