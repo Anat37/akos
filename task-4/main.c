3d3cc8071e438d976e6845281a0980d2d9df12a1
Kozlov Nikita <kozlovzxc@gmail.com>
2014-10-13 12:15:56 +0400

    wrote task-4

--- task-4/main.c
+++ task-4/main.c
@@ -20,8 +20,8 @@ char is_there(char* arr,int arrlen)
     int i;
     for(i=0;i<arrlen;i++)
         if (arr[i]==0)
-            return 0;
-    return 1;
+            return 1;
+    return 0;
 }
 
 #define stringlen 256
@@ -31,32 +31,34 @@ int main(int argv, char** argc)
     FILE *in_file = fopen(argc[1],"r");
     FILE *out_file = fopen(argc[2],"w");
     char *tmps = (char*)malloc(stringlen*sizeof(char));
-    
-    char *mins = (char*)malloc(stringlen*sizeof(char));
     int mini = 0;
+    int filepos = 0;
+    char *mins = (char*)malloc(stringlen*sizeof(char));
     fgets(mins,stringlen,in_file);
-    int filelen = 0;
-    
-    
+   
     while (fgets(tmps,stringlen,in_file))
     {
-        filelen += 1;
+        filepos += 1;
         if (strcmp(mins,tmps) > 0)
         {
             strcpy(mins,tmps);
             mini = filelen;
         }
     }
-    //printf("len = %i\nstring = %spos = %i\n",filelen+1,mins,mini);
+    printf("len = %i\nstring = %spos = %i\n",filelen+1,mins,mini);
     
-    char *used = (char*)calloc(filelen,sizeof(char));
+    char *used = (char*)calloc(filepos,sizeof(char));
     used[mini] = 1;
     fprintf(out_file,"%s",mins);
     
-    while
-    
+    while (is_there(used,filelen))
+    {
+        filepos = 0;
+        fseek(in_file, 0, SEEK_SET);
+        while
+    }
 
-
+    free(mins);
     free(tmps);
     close_files(2,in_file,out_file);
     return 0;
