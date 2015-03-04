5d4d7b927b149eb674524b13eaf917d4967df217
Kozlov Nikita <kozlovzxc@gmail.com>
2015-03-04 23:17:57 +0300

    read and write from binary and text

--- C++/matrix/Complex.cpp
+++ C++/matrix/Complex.cpp
@@ -1,36 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#include "complex.h"
 
-class Complex{
-    double re,im;
-public:
-    Complex(double in_re = 0,double in_im = 0);
-    void print();
-    Complex neg();
-    double module();
+using namespace prac_matrices;
 
-    void operator=(Complex sample);
-    Complex operator+(double sample);
-    Complex operator-(double sample);
-    Complex operator*(double sample);
-    Complex operator/(double sample);
-    
-    Complex operator+(Complex sample);
-    Complex operator-(Complex sample);
-    Complex operator*(Complex sample);
-    Complex operator/(Complex sample);
-};
-
-Complex::Complex(double re,double im)
+Complex::Complex(double re ,double im)
 {
     this->re = re;
     this->im = im;
-}
-
-char* Complex::print()
-{
-    char tmp[128]
-    printf("%.2lf + i*%.2lf\n",re,im);
 }
 
 Complex Complex::neg()
@@ -48,24 +25,6 @@ void Complex::operator=(Complex sample)
 {
     this->re = sample.re;
     this->im = sample.im;
-}
-
-Complex Complex::operator+(double sample)
-{
-    Complex tmp(this->re+sample, this->im);
-    return tmp;
-}
-
-Complex Complex::operator-(double sample)
-{
-    Complex tmp(this->re - sample, this->im);
-    return tmp;
-}
-
-Complex Complex::operator*(double sample)
-{
-    Complex tmp(this->re*sample,this->im*sample);
-    return tmp;
 }
 
 Complex Complex::operator/(double sample)
@@ -95,13 +54,4 @@ Complex Complex::operator*(Complex sample)
 Complex Complex::operator/(Complex sample)
 {
     return (*this)*sample.neg()/(sample*sample.neg()).re;
-}
-
-int main()
-{
-    Complex a(1,2),b(3,4);
-    Complex c = b;
-    c.print();
-    printf("%0.2lf\n",c.module());
-    return 0;
 }
